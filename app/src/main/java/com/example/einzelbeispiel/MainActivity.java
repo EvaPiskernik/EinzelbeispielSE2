b4d78cbf001e914a073f4866bdd712f14ab250ea
Eva Piskernik <eva.piskernik@gmail.com>
2024-03-11 00:10:01 +0100

    implemented calculation with studentNR

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -38,6 +38,27 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        findViewById(R.id.buttonCalculation).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String studentNr_et = ((EditText)(findViewById(R.id.editTextNumber))).getText().toString();
+                StringBuilder builder = new StringBuilder();
+                for(int i = 0; i<studentNr_et.length(); i++){
+                    if(i % 2 == 1){
+                        int digit = studentNr_et.charAt(i) - '0';
+                        char letter = (char) ( 'a' + digit);
+                        builder.append(letter);
+
+                    }
+                    else {
+                        builder.append(studentNr_et.charAt(i));
+                    }
+                    String result = builder.toString();
+                    ((TextView)findViewById(R.id.serverAnswer_Tv)).setText(result);
+                }
+
+            }
+        });
 
     }
 
